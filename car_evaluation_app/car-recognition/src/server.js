ec334576f89dd0d40a8216dfbe70f6a42fc5a047
motwave <archer.yiyang@gmail.com>
2023-05-08 14:21:11 -0700

    added backend

--- car_evaluation_app/car-recognition/src/server.js
+++ car_evaluation_app/car-recognition/src/server.js
@@ -1,19 +1,61 @@
 const express = require('express');
 const multer = require('multer');
 const cors = require('cors');
+const fs = require('fs');
+const path = require('path');
+const { PythonShell } = require('python-shell');
+
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-const storage = multer.memoryStorage();
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, 'uploads/');
+  },
+  filename: (req, file, cb) => {
+    cb(null, Date.now() + path.extname(file.originalname));
+  },
+});
+
 const upload = multer({ storage });
 
 app.post('/api/recognizeCar', upload.single('image'), async (req, res) => {
-  // Process the image with the machine learning model here
-  // and return the car model name
-  res.json({ carModel: 'Car Model Name' });
+  const imagePath = req.file.path;
+  try {
+    const carModel = await recognizeCarModel(imagePath);
+    res.json({ carModel });
+  } catch (error) {
+    res.status(500).json({ error: 'Error recognizing car model' });
+  } finally {
+    // Clean up the uploaded image file
+    fs.unlink(imagePath, (err) => {
+      if (err) console.error('Error deleting image file:', err);
+    });
+  }
 });
+
+async function recognizeCarModel(imagePath) {
+  const options = {
+    mode: 'text',
+    pythonOptions: ['-u'],
+    scriptPath: './',
+    args: [imagePath],
+  };
+
+  return new Promise((resolve, reject) => {
+    PythonShell.run('recognize_car_model.py', options, (err, results) => {
+      if (err) {
+        console.error('Error running Python script:', err);
+        reject(err);
+      } else {
+        const carModel = results[0]; // Assuming the script prints only the car model name
+        resolve(carModel);
+      }
+    });
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
