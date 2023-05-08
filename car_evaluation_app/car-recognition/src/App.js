adde30d0a5e578cce731330c1decfe13c8123619
motwave <archer.yiyang@gmail.com>
2023-05-08 14:08:12 -0700

    webpage functions done

--- car_evaluation_app/car-recognition/src/App.js
+++ car_evaluation_app/car-recognition/src/App.js
@@ -1,24 +1,42 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
+import './App.css';
 
 function App() {
   const [image, setImage] = useState(null);
   const [carModel, setCarModel] = useState('');
+  const [loading, setLoading] = useState(false);
+  const [previewImage, setPreviewImage] = useState(null);
 
   const handleImageChange = (e) => {
-    setImage(e.target.files[0]);
+    const file = e.target.files[0];
+    setImage(file);
+
+    if (file) {
+      setPreviewImage(URL.createObjectURL(file));
+    } else {
+      setPreviewImage(null);
+    }
   };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
+    setLoading(true);
     const formData = new FormData();
     formData.append('image', image);
 
-    const response = await fetch('/api/recognizeCar', {
-      method: 'POST',
-      body: formData,
-    });
-    const data = await response.json();
-    setCarModel(data.carModel);
+    try {
+      const response = await fetch('/api/recognizeCar', {
+        method: 'POST',
+        body: formData,
+      });
+      const data = await response.json();
+      setCarModel(data.carModel);
+    } catch (error) {
+      console.error('Error recognizing car model:', error);
+      setCarModel('Error recognizing car model');
+    } finally {
+      setLoading(false);
+    }
   };
 
   return (
@@ -26,6 +44,24 @@ function App() {
       <h1>Car Model Recognition</h1>
       <form onSubmit={handleSubmit}>
         <input type="file" onChange={handleImageChange} />
-        <button type="submit">Recognize Car Model</button>
+        <button type="submit" disabled={!image || loading}>
+          {loading ? 'Recognizing...' : 'Recognize Car Model'}
+        </button>
       </form>
-      {carModel
+      {previewImage && (
+        <div>
+          <h2>Uploaded Image:</h2>
+          <img src={previewImage} alt="Uploaded car" style={{ maxWidth: '100%' }} />
+        </div>
+      )}
+      {carModel && (
+        <div>
+          <h2>Car Model:</h2>
+          <p>{carModel}</p>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default App;
